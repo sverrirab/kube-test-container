c77b88f784fe0fc7c81e07136b8a1ffa086ce816
Sverrir Á. Berg <sab@keilir.com>
2017-02-15 17:37:23 +0000

    Adding margins in UI
    Minor script fixes

--- build.sh
+++ build.sh
@@ -2,4 +2,11 @@
 
 GOOS=linux go build -a --ldflags '-extldflags "-static"' -tags kube-test-container ./cmd/kube-test-container.go
 
-docker build -t sverrirab/kube-test-container:v1.0 .
+IMAGE_NAME=sverrirab/kube-test-container
+
+docker build -t sverrirab/kube-test-container .
+
+if [ -n "$1" ] ; then
+    echo "Tagging with $1"
+    docker tag sverrirab/kube-test-container sverrirab/kube-test-container:$1
+fi
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Browse to http://localhost:8000/ to test"
+
 docker run -it -p 8000:8000 sverrirab/kube-test-container
 
